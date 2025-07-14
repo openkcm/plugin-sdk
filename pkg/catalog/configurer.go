1d7d77a3a4d781eabd243bf20b5645dbe3b2210f
Nicolae Nicora <n.nicora@sap.com>
2025-07-14 16:32:11 +0200

    remove unused resources

--- pkg/catalog/configurer.go
+++ pkg/catalog/configurer.go
@@ -2,7 +2,6 @@ package catalog
 
 import (
 	"context"
-	"log/slog"
 
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -35,34 +34,5 @@ func (c *configurer) Configure(ctx context.Context) error {
 	case codes.OK:
 		return nil
 	}
-	return err
-}
-
-type Configurer interface {
-	Configure(ctx context.Context, configuration string) error
-}
-
-type ConfigurerFunc func(ctx context.Context, configuration string) error
-
-func (fn ConfigurerFunc) Configure(ctx context.Context, configuration string) error {
-	return fn(ctx, configuration)
-}
-
-type configurerV1 struct {
-	configv1.ConfigServiceClient
-}
-
-var _ Configurer = (*configurerV1)(nil)
-
-func (v1 *configurerV1) InitInfo(PluginInfo) {
-}
-
-func (v1 *configurerV1) InitLog(*slog.Logger) {
-}
-
-func (v1 *configurerV1) Configure(ctx context.Context, yamlConfiguration string) error {
-	_, err := v1.ConfigServiceClient.Configure(ctx, &configv1.ConfigureRequest{
-		YamlConfiguration: yamlConfiguration,
-	})
 	return err
 }
