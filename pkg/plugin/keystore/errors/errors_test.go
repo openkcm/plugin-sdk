d403a26dbe069ca901e1207f4eacc5186a708792
Nghia <minh-nghia.nguyen@sap.com>
2025-10-29 13:25:25 +0000

    refactor: update keystore plugin errors (#71)

--- pkg/plugin/keystore/errors/errors_test.go
+++ pkg/plugin/keystore/errors/errors_test.go
@@ -1,24 +1,81 @@
 package errors_test
 
 import (
+	"errors"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"google.golang.org/grpc/status"
 
 	keystoreErrs "github.com/openkcm/plugin-sdk/pkg/plugin/keystore/errors"
 )
 
-func TestStatusProviderAuthenticationErrorWithDetails(t *testing.T) {
-	err := keystoreErrs.NewGrpcErrorWithReason(
-		keystoreErrs.StatusProviderAuthenticationError,
-		"Invalid credentials",
+func TestNewGrpcErrorWithDetails(t *testing.T) {
+	metadata := map[string]string{"foo": "bar"}
+	err := keystoreErrs.NewGrpcErrorWithDetails(
+		keystoreErrs.StatusInvalidKeyAccessData,
+		keystoreErrs.ReasonMissingFields,
+		metadata,
 	)
+	reason, meta := keystoreErrs.GetDetails(err)
+	assert.Equal(t, keystoreErrs.ReasonMissingFields, reason)
+	assert.Equal(t, metadata, meta)
+}
 
-	extractedReason := keystoreErrs.GetReason(err)
-	if extractedReason != "Invalid credentials" {
-		t.Errorf("Expected reason 'Invalid credentials', got '%s'", extractedReason)
+func TestWithMetadata_NewMetadata(t *testing.T) {
+	err := keystoreErrs.NewGrpcErrorWithDetails(
+		keystoreErrs.StatusInvalidKeyAccessData,
+		keystoreErrs.ReasonMissingFields,
+		nil,
+	)
+	newMeta := map[string]string{
+		keystoreErrs.MetadataMissingFields: "keyId,accessData",
 	}
+	mergedErr := keystoreErrs.WithMetadata(err, newMeta)
+	_, meta := keystoreErrs.GetDetails(mergedErr)
+	assert.Equal(t, map[string]string{
+		"missingFields": "keyId,accessData",
+	}, meta)
+}
 
-	if !keystoreErrs.IsStatus(err, keystoreErrs.StatusProviderAuthenticationError) {
-		t.Errorf("Error does not match StatusProviderAuthenticationError")
+func TestWithMetadata_MergesMetadata(t *testing.T) {
+	origMeta := map[string]string{"foo": "bar"}
+	err := keystoreErrs.NewGrpcErrorWithDetails(
+		keystoreErrs.StatusInvalidKeyAccessData,
+		keystoreErrs.ReasonMissingFields,
+		origMeta,
+	)
+	newMeta := map[string]string{
+		keystoreErrs.MetadataMissingFields: "keyId,accessData",
 	}
+	mergedErr := keystoreErrs.WithMetadata(err, newMeta)
+	_, meta := keystoreErrs.GetDetails(mergedErr)
+	assert.Equal(t, map[string]string{
+		"foo":           "bar",
+		"missingFields": "keyId,accessData",
+	}, meta)
+}
 
+func TestIsStatus(t *testing.T) {
+	err := keystoreErrs.NewGrpcErrorWithDetails(
+		keystoreErrs.StatusKeyNotFound,
+		"NO_LONGER_EXISTS",
+		nil,
+	)
+	assert.True(t, keystoreErrs.IsStatus(err, keystoreErrs.StatusKeyNotFound))
+	assert.False(t, keystoreErrs.IsStatus(err, keystoreErrs.StatusInvalidKeyAccessData))
+}
+
+func TestGetDetails_NoDetails(t *testing.T) {
+	err := status.New(1, "no details").Err()
+	reason, meta := keystoreErrs.GetDetails(err)
+	assert.Empty(t, reason)
+	assert.Nil(t, meta)
+}
+
+func TestGetDetails_NormalError(t *testing.T) {
+	err := errors.New("normal error")
+	reason, meta := keystoreErrs.GetDetails(err)
+	assert.Equal(t, "", reason)
+	assert.Nil(t, meta)
 }
