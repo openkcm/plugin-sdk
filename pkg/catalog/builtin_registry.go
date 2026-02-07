fc81d6305b8dcc13b6c1e63c0465019d5b278050
nnicora <75667951+nnicora@users.noreply.github.com>
2026-02-07 17:43:24 +0100

    fix: introduce interface for built in plugins (#88)

--- pkg/catalog/builtin_registry.go
+++ pkg/catalog/builtin_registry.go
@@ -1,26 +1,26 @@
 package catalog
 
 type BuiltInPluginRegistry interface {
-	Register(plugin BuiltIn)
-	Get() []BuiltIn
+	Register(plugin BuiltInPlugin)
+	Get() []BuiltInPlugin
 }
 
 type buildInRegistry struct {
-	plugins []BuiltIn
+	plugins []BuiltInPlugin
 }
 
 func DefaultBuiltInPluginRegistry() BuiltInPluginRegistry {
 	return &buildInRegistry{
-		plugins: make([]BuiltIn, 0),
+		plugins: make([]BuiltInPlugin, 0),
 	}
 }
 
-func (r *buildInRegistry) Register(plugin BuiltIn) {
+func (r *buildInRegistry) Register(plugin BuiltInPlugin) {
 	r.plugins = append(r.plugins, plugin)
 }
 
-func (r *buildInRegistry) Get() []BuiltIn {
-	plugins := make([]BuiltIn, 0, len(r.plugins))
+func (r *buildInRegistry) Get() []BuiltInPlugin {
+	plugins := make([]BuiltInPlugin, 0, len(r.plugins))
 	plugins = append(plugins, r.plugins...)
 	return plugins
 }
--- pkg/catalog/builtin_registry_test.go
+++ pkg/catalog/builtin_registry_test.go
@@ -23,14 +23,14 @@ func TestBuiltInRegistry(t *testing.T) {
 
 		reg := DefaultBuiltInPluginRegistry()
 
-		p1 := BuiltIn{Name: "plugin-1"}
-		p2 := BuiltIn{Name: "plugin-2"}
+		p1 := &builtInPluginStruct{name: "plugin-1"}
+		p2 := &builtInPluginStruct{name: "plugin-2"}
 
 		reg.Register(p1)
 		reg.Register(p2)
 
 		got := reg.Get()
-		want := []BuiltIn{p1, p2}
+		want := []BuiltInPlugin{p1, p2}
 
 		if !reflect.DeepEqual(got, want) {
 			t.Fatalf("unexpected plugins\nwant: %#v\ngot:  %#v", want, got)
@@ -42,14 +42,14 @@ func TestBuiltInRegistry(t *testing.T) {
 
 		reg := DefaultBuiltInPluginRegistry()
 
-		p1 := BuiltIn{Name: "plugin-1"}
+		p1 := &builtInPluginStruct{name: "plugin-1"}
 		reg.Register(p1)
 
 		plugins := reg.Get()
-		plugins[0] = BuiltIn{Name: "mutated"}
+		plugins[0] = &builtInPluginStruct{name: "mutated"}
 
 		got := reg.Get()
-		if got[0].Name != p1.Name {
+		if got[0].Name() != p1.Name() {
 			t.Fatalf(
 				"registry was mutated via Get(); want %+v, got %+v",
 				p1,
