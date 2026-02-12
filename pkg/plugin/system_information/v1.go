f051b1c50475b41c9d0d79dbf12c771b34dd762c
Nicolae Nicora <n.nicora@sap.com>
2026-02-12 12:37:04 +0100

    improve the code

--- pkg/plugin/system_information/v1.go
+++ pkg/plugin/system_information/v1.go
@@ -23,9 +23,21 @@ func (v1 *V1) ServiceInfo() api.Info {
 }
 
 func (v1 *V1) GetSystemInfo(ctx context.Context, req *systeminformation.GetSystemInfoRequest) (*systeminformation.GetSystemInfoResponse, error) {
+	// Convert your API request type to the gRPC enum
+	var grpcType systeminformationv1.RequestType
+
+	switch req.Type {
+	case systeminformation.UnspecifiedType:
+		grpcType = systeminformationv1.RequestType_REQUEST_TYPE_SYSTEM
+	case systeminformation.SubaccountType:
+		grpcType = systeminformationv1.RequestType_REQUEST_TYPE_SUBACCOUNT
+	default:
+		grpcType = systeminformationv1.RequestType_REQUEST_TYPE_UNSPECIFIED
+	}
+
 	in := &systeminformationv1.GetRequest{
-		Id: req.ID,
-		//Type: toGRPCType(req.Type),
+		Id:   req.ID,
+		Type: grpcType,
 	}
 	grpcResp, err := v1.Get(ctx, in)
 	if err != nil {
